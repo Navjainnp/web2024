b5fa18cf19b5b685385363647ee19d4b7a9049da
navjainnp <navjain.np@gmail.com>
2024-08-29 19:07:59 +0545

    templates- add.html layout.html in task app

--- django/lecture3/tasks/views.py
+++ django/lecture3/tasks/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
-tasks = ["foo", "bar", "baz"]
+tasks = ["foo", "bar", "baz"] #global variable
 
 # Create your views here.
 def index(request):
     return render(request, "tasks/index.html", {
-        "tasks": tasks
+        "tasks": tasks #key : value pair
     })
+
+def add(request):
+    return render(request, "tasks/add.html")
     
