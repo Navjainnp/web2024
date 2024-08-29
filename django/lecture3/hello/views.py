73d630b373172ed70cb5704010c245b7b40c31b8
navjainnp <navjain.np@gmail.com>
2024-08-29 12:42:34 +0545

    templates render instead of httpResponse string

--- django/lecture3/hello/views.py
+++ django/lecture3/hello/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request): #index is function & request is argument-represents http request that the user made to access web serve. 
-    return HttpResponse("hello, world!")
+    return render(request, "hello/index.html")
 
 def navjain(request):
     return HttpResponse("hello Nav Jain")
 
 def greet(request, name):
-    return HttpResponse(f"hello, {name.capitalize()}")
+    #return HttpResponse(f"hello, {name.capitalize()}")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
